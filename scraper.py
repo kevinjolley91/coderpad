1e2f5324d5f6caf896ee61172d712af393cca8f7
kevinjolley91 <jolley427@gmail.com>
2024-03-27 16:47:06 -0400

    using map and get_text methods to get the data we're after

--- scraper.py
+++ scraper.py
@@ -1,7 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://pixelford.com/blog/"
 import random
 random_number = random.randint(1, 999999999999)
 response = requests.get(url, headers = {'user-agent': f'Hello {random_number}'})
-print(response.content)
+html = response.content
+soup = BeautifulSoup(html, 'html.parser')
+a_tags = soup.find_all('a', class_="entry-title-link")
+
+for a_tag in a_tags:
+    print(a_tag.get_text())
+    
+titles = list(map(lambda a_tag: a_tag.get_text(), a_tags))
+
+print(titles)
