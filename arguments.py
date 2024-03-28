26ed27cae0240150c5c9bc5dbcf42ad625c8496f
kevinjolley91 <jolley427@gmail.com>
2024-03-28 14:31:52 -0400

    learning about including arguments from the command line

---
+++ arguments.py
@@ -0,0 +1,16 @@
+import sys
+    
+# Make a script that mulitplies all the number arguments passed into it
+
+total = 1
+del(sys.argv[0])
+for argument in sys.argv:
+    try:
+        number = float(argument)
+        total *= number
+    except Exception as e:
+        print(e)
+        print("Only numbers can be provided.")
+        sys.exit(1)
+    
+print(total)
