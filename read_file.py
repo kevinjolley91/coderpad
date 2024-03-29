84063137e3e2aea349aa37a6b93ee1f2647351e5
kevinjolley91 <jolley427@gmail.com>
2024-03-29 13:59:13 -0400

    learning how to read through files with code

---
+++ read_file.py
@@ -0,0 +1,27 @@
+# file = open("cheese.txt", "r")
+
+# file_text = file.read()
+# print(file_text)
+
+# lines = file.readlines()
+# print(lines)
+
+# for line in file:
+#     print(line)
+
+# file.close()
+
+
+
+
+# Create a file numbers.txt that has a few lines of numbers.
+# Multipy all the number together and print the result.
+file = open("numbers.txt", "r")
+
+total = 1
+for line in file:
+    total *= int(line)
+    
+print(total)
+
+file.close()
