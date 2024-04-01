92a82722e9a34f56c9179ffc44069d5d22e9babb
kevinjolley91 <jolley427@gmail.com>
2024-04-01 13:05:13 -0400

    learning about editing txt files through code

--- read_file.py
+++ read_file.py
@@ -1,27 +1,27 @@
-# file = open("cheese.txt", "r")
+file = open("cheese.txt", "r")
 
-# file_text = file.read()
-# print(file_text)
+file_text = file.read()
+print(file_text)
 
-# lines = file.readlines()
-# print(lines)
+lines = file.readlines()
+print(lines)
 
-# for line in file:
-#     print(line)
+for line in file:
+    print(line)
 
-# file.close()
+file.close()
 
 
 
 
 # Create a file numbers.txt that has a few lines of numbers.
 # Multipy all the number together and print the result.
-file = open("numbers.txt", "r")
+# file = open("numbers.txt", "r")
 
-total = 1
-for line in file:
-    total *= int(line)
+# total = 1
+# for line in file:
+#     total *= int(line)
     
-print(total)
+# print(total)
 
-file.close()
+# file.close()
